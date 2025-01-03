4e06f4db8c9a8fe2cb1a4c8a72ddcec2819904ee
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-04 05:10:45 +0530

    python variables

---
+++ variables.py
@@ -0,0 +1,24 @@
+x = 5
+y = 'Deepak'
+print(x)
+print(y)
+
+# casting
+x = str(3)
+y = int(3)
+z = float(3)
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Get the type
+x = 5
+y = 'Deepak'
+print(type(x))
+print(type(y))
+
+# Single or double quotes
+x = "Deepak"
+y = 'Deepak'
+print(x, type(x), end = ' \n')
+print(y, type(y), end = ' ')
