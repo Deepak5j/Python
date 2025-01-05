1673c01bef04811e543970b8804337545ca980b2
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-05 17:27:26 +0530

    datatypes - built in, type(), setting data value

---
+++ 2data_types.py
@@ -0,0 +1,20 @@
+print("1. Built in data types.............")
+print("text type values:- str")
+print("numeric type values:- int, float, complex")
+print("sequence type values:- list, tuple, range")
+print("mapping type values:- dict")
+print("set type values:- set, frozenset")
+print("boolean type values:- bool")
+print("binary type values:- bytes, bytearray, memoryview")
+print("None type values:- None")    
+
+print("\n2. Getting the data type.............")
+x = "Deepak Gautam"
+print("'Deepak Gautam' is ",type(x))
+x = 5
+print("5 is ",type(x))
+x = 5.5
+print("5.5 is ",type(x))
+
+print("\n3. Setting the data type.............")    
+x = str("Deepak Gautam")
