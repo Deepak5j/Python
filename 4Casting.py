6c7c6d53f7cd577fbdc4f85ea4fdae672eba3526
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-07 04:54:09 +0530

    For casting constructor function :- int(), float(), str()

---
+++ 4Casting.py
@@ -0,0 +1,29 @@
+print("For casting constructor function :- int(), float(), str()")
+# integers
+x = int(1) # 1
+y = int(2.5) #2
+z = int('3') #3
+print('1. Inegers casting')
+print(type(x))
+print(type(y))
+print(type(z))
+
+#float
+x = float(1) # 1.0
+y = float(2.5) # 2.5
+z = float("3") # 3.0
+w = float('4.5') # 4.5
+print('2. float casting')
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
+
+#string
+x = str('s1') # 's1'
+y = str(2) # '2'
+z = str(3.5) # '3.5'
+print('3. String casting')
+print(type(x))
+print(type(y))
+print(type(z))
