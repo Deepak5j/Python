aada7264d4f8b9b5d1146e4cd8940b5febc556df
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-06 22:26:32 +0530

    Number, int, float, complex

--- 2data_types.py
+++ 2data_types.py
@@ -45,6 +45,3 @@ n = memoryview(bytes(7)) #memoryview
 t = None #None
 for i in [a1,a2,b1,b2,c1,c2,d1,d2,e1,e2,f1,f2,g1,g2,h1,h2,i1,i2,j,k1,k2,l1,l2,m,n,t]:
     print(i, type(i))
-
-
-
---
+++ 3numbers.py
@@ -0,0 +1,20 @@
+print("1. 3 types are int, float, complex")
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+p = -87.7e100 # float 
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(p))
+
+print("2. Complex numbers are written with a 'j' as the imaginary part")
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
