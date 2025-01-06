f11c7ef7c60a773875bf745219d5c51739c39a9d
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-06 22:29:20 +0530

    Number conversion, random numebr

--- 3numbers.py
+++ 3numbers.py
@@ -18,3 +18,30 @@ print(type(x))
 print(type(y))
 print(type(z))
 
+print("3. To convert from one type to another, you can use the int(), float(), and complex() functions")
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+print("You cannot convert complex numbers into another number type.")
+
+print("4. Random number")
+import random
+print(random.randrange(100,1000))
