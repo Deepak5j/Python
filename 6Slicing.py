88f9f178de5fdc0600dd44b0c6d576cbb7c56285
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-07 11:36:48 +0530

    Slicing, index to index, start to index, index to end

---
+++ 6Slicing.py
@@ -0,0 +1,21 @@
+# defining a string for operation
+a = 'Hello, World!'
+
+print('1. Slicing a word from string')
+print(a[7:12])
+
+print('\n2. Slicing from start to index')
+print(a[:5])
+
+print('\n Slicing from index to end')
+print(a[7:])
+
+print('\n Negative indexing')
+'''
+h    e    l    l   o   ,   _   W   o   r   l   d
+0    1    2    3   4   5   6   7   8   9   10  11
+-12  -11  -10  -9  -8  -7  -6  -5  -4  -3  -2  -1 
+'''
+
+
+
