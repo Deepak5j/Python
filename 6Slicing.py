2fc4eb554f6545b611bd5252a929faf8fef579e6
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-07 11:59:20 +0530

    slicing, negative index

--- 6Slicing.py
+++ 6Slicing.py
@@ -1,21 +1,30 @@
 # defining a string for operation
 a = 'Hello, World!'
 
-print('1. Slicing a word from string')
+print('1.Slicing from a index to index')
 print(a[7:12])
 
-print('\n2. Slicing from start to index')
+print('\n2.Slicing from start to index')
 print(a[:5])
 
-print('\n Slicing from index to end')
+print('\nSlicing from index to end')
 print(a[7:])
 
-print('\n Negative indexing')
-'''
-h    e    l    l   o   ,   _   W   o   r   l   d
-0    1    2    3   4   5   6   7   8   9   10  11
--12  -11  -10  -9  -8  -7  -6  -5  -4  -3  -2  -1 
-'''
+print('\nNegative indexing')
+doc = '''
+h     e    l    l   o   ,   _   W   o   r   l   d  !
+0     1    2    3   4   5   6   7   8   9   10  11 12
+-13 -12  -11  -10  -9  -8  -7  -6  -5  -4  -3  -2  -1 
 
+In Python slicing, the general syntax is a[start:stop]
+
+For last chracter ==> a[12:13] or a[-1:]
+'''
+print(doc) 
+print(a[-13:-8])
+print(a[-6:-1])
+print(a[-1:]) # starting from last index to blank, means end.
+
+ 
 
 
