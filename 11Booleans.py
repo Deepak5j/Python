db8f7c6053173348872c77414e771d540099add5
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-08 17:00:34 +0530

    Arithmetic Operators

--- 11Booleans.py
+++ 11Booleans.py
@@ -14,3 +14,42 @@ print('value and variable evaluation')
 print(bool('Hello'))
 print(bool(15))
 
+print('\n2.Most Values are True')
+print(bool('abc'))
+print(bool(123))
+print(bool(['apple', 'banana', 'cherry']))
+
+print('\n3.Some Values are False')
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+
+print('\n4.Functions can Return a Boolean')
+def myFunction() :
+    def __len__(self):
+        return 0
+obj = myFunction()
+print(bool(obj))
+
+print('\n5.Using the bool() Function')
+def myFunction() :
+    return True
+print(myFunction())
+
+
+def myFunction() :
+    return True
+if myFunction():
+    print("YES!")
+else:
+    print("NO!")
+    
+print('\n6. isinstance() Function')
+x = 200
+print(isinstance(x, int))
+print(isinstance(x, str))
+
---
+++ 12Operator.py
@@ -0,0 +1,30 @@
+print(10 + 5)
+
+print('1.Arithmetic Operators')
+print('Addition',10 + 5)
+print('Subtraction',10 - 5)
+print('Multiplication',10 * 5)
+print('Division',10 / 5)
+print('Floor Division',10 // 3)
+print('Modulus',10 % 3)
+print('Exponentiation',10 ** 3)
+
+print('\n2.Assignment Operators')
+print('=')
+print('+=')
+print('-=')
+print('*=')
+print('/=')
+print('//=')
+print('%=')
+
+
+
+
+
+
+
+
+
+
+
