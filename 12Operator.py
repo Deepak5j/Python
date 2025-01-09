5959be6abeba841ac4bcc92e78f700f223428d02
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-10 05:07:06 +0530

    Membership Operators

--- 12Operator.py
+++ 12Operator.py
@@ -40,13 +40,21 @@ print('or 1>5 or 3<10', 1>5 or 3<10)
 print('not 1<5', not(1<5))
 
 print('\n5.Identity Operators')
-print('is')
+print('is not')
 x = ["apple", "banana"]
 y = ["apple", "banana"]
 z = x
 print(x is not z)
 print(x is not y)
 print(x != y)
+print('is')
+print(x is z)
+print(x is y)
+print(x == y)
+
+print('\n6.Membership Operators')
+print("banana" in x)
+print("pineapple" not in x)
 
 
 
