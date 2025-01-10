fc999806c9291e5438e71525fceb2eb48c889b51
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-10 11:41:11 +0530

    list constructor

--- 13List.py
+++ 13List.py
@@ -12,3 +12,22 @@ print(ls[1])
 
 print('\n5. Negative Indexing, at index -1')
 print(ls[-1])
+
+print('\n6.Data types')
+ls1 = ["apple", "banana", "cherry"]
+ls2 = [1, 5, 7, 9, 3]
+ls3 = [True, False, False]
+print('String List: ', ls1)
+print('Integer List: ', ls2)
+print('Boolean List: ', ls3)
+
+print('\n7. type()')
+print(type(ls1))
+print(type(ls2))
+print(type(ls3))
+
+print('\n8. The list() Constructor')
+ls4 = list(("apple", "banana", "cherry"))
+print(ls4)
+ls5 = list((1,2))
+print(ls5)
