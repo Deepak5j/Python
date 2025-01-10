bbec536418ff01ddf9c1bfc3478f2b4b31ef2e16
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-10 09:51:54 +0530

    list

---
+++ 13List.py
@@ -0,0 +1,14 @@
+print('1. List')
+ls = ['apple', 'banana', 'cherry']
+print(ls)
+
+ls = ["apple", "banana", "cherry", "apple", "cherry"]
+print('\n2.list with dublicate items: \n', ls)
+
+print('\n3.Length of above list: ', len(ls))
+
+print('\n4. Access Items, using index, at index 1')
+print(ls[1])
+
+print('\n5. Negative Indexing, at index -1')
+print(ls[-1])
