34102e4c2ceaae6fa7e5de5d81688ce0a75654bc
Deepak Gautam <DEEPAKGAUTAMA5J@GMAIL.COM>
2025-01-13 05:18:23 +0530

    list comprehension, inerrable, expression

--- 13List.py
+++ 13List.py
@@ -98,5 +98,26 @@ print('\n\n21. List Comprehension')
 ls7 = [x for x in ls3]
 print(ls7)
 
+print('\n22. List Comprehension')
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+#newlist = [expression for item in iterable if condition == True]
+newlist = [x for x in fruits if "a" in x]
+print(fruits)
+print(newlist)
 
+print('\n23. iterable')
+newlist = [x for x in range(10)]
+print(newlist)
+newlist = [x for x in range(10) if x < 5]
+print(newlist)
 
+print('\n24. iterable expresion')
+newlist = [x.upper() for x in fruits]
+print(newlist)
+
+print('\n25. expressiom')
+newlist = ['hello' for x in fruits]
+print(newlist)
+
+newlist = [x if x != "banana" else "orange" for x in fruits]
+print(newlist)
